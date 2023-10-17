ac1e5f6809ff5fdaf6faae3ce8e7a18259339b98
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-17 16:08:03 +0200

    Run application

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -3,6 +3,5 @@ package org.lessons.java;
 public class StampaSaluto {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
-        System.out.println("Hello Java Eccomi!");
     }
 }
